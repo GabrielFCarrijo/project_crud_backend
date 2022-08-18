4918de80e52f66a8f8b6d28ca6dac5012182bbc6
GabrielCarrijoF <carrijogit2003@gmail.com>
2022-08-17 23:50:03 -0300

    refactor: database exception

--- src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
+++ src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscatalog.services;
 
 import com.devsuperior.dscatalog.repositories.ProductRepository;
+import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -24,14 +26,28 @@ public class ProductServiceTests {
 
   private long existingId;
   private long notExistingId;
+  private long dependentId;
 
   @BeforeEach
   void variableInitialization() {
+
     existingId = 1l;
     notExistingId = 1000l;
+    dependentId = 4l;
+
     Mockito.doNothing().when(productRepository).deleteById(existingId);
-    Mockito.doThrow(EmptyResultDataAccessException.class).when(productRepository)
-        .deleteById(notExistingId);
+    Mockito.doThrow(EmptyResultDataAccessException.class).when(productRepository).deleteById(notExistingId);
+    Mockito.doThrow(DataIntegrityViolationException.class).when(productRepository).deleteById(dependentId);
+  }
+
+  @Test
+  public void givenDeleteShouldThrowDataIntegrityViolationExceptionWhenIdNotExists() {
+
+    Assertions.assertThrows(DatabaseException.class, () -> {
+      productService.delete(dependentId);
+    });
+
+    Mockito.verify(productRepository).deleteById(dependentId);
   }
 
   @Test
